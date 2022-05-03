b489504a82851d04677cf0dc629cc811a471d08e
afeiship <1290657123@qq.com>
2022-05-03 22:14:26 +0800

    fix: upt dir files

--- generators/app/templates/build/scripts.js
+++ generators/app/templates/build/scripts.js
@@ -2,14 +2,15 @@
   'use strict';
 
   const gulp = require('gulp');
+  const tsconfig = require('../tsconfig.json');
   const $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
   });
-  const tsconfig = require('../tsconfig.json');
+
 
   gulp.task('scripts:cjs', function () {
     return gulp
-      .src('src/*.ts')
+      .src('src/**/*.ts')
       .pipe($.replace('export default ', 'export = '))
       .pipe($.jswork.pkgHeader())
       .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'commonjs' }))
@@ -19,7 +20,7 @@
 
   gulp.task('scripts:esm', function () {
     return gulp
-      .src('src/*.ts')
+      .src('src/**/*.ts')
       .pipe($.jswork.pkgHeader())
       .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'esnext' }))
       .pipe(gulp.dest('dist/esm'))
