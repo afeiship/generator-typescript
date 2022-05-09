dd4fd94036305c2e60f113a1229345f5b20320df
afeiship <1290657123@qq.com>
2022-05-09 22:57:02 +0800

    wip

--- generators/esbuild/templates/src/index.ts
+++ generators/esbuild/templates/src/index.ts
@@ -1,4 +1,5 @@
 // https://jamesthom.as/2021/05/setting-up-esbuild-for-typescript-libraries/
+// https://souporserious.com/bundling-typescript-with-esbuild-for-npm/
 export default () => {
   console.log('Hello World');
 }
