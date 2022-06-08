06ab6cfdd082ab154d9bb1bba4e7e0806abbc4d2
afeiship <1290657123@qq.com>
2022-06-08 22:19:18 +0800

    feat: test/jest upt spec

---
+++ generators/esbuild/templates/__tests__/index.spec.js
@@ -0,0 +1,15 @@
+const { CraEnvs } = require('../dist/index.cjs');
+
+describe('test cra-envs', () => {
+  test('cra-envs: get all', function () {
+    const res = CraEnvs.get();
+    expect(res).toEqual({
+      beta: {
+        base_url: 'https://site-predict-platform.beta.saybot.net',
+      },
+      staging: {
+        base_url: 'https://site-predict-platform.staging.saybot.net',
+      },
+    });
+  });
+});
