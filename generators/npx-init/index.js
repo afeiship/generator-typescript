4eab1c36e27050af091508a4167c853c4d9c8ca3
afeiship <1290657123@qq.com>
2022-12-04 08:22:01 +0800

    feat: npx-init

---
+++ generators/npx-init/index.js
@@ -0,0 +1,30 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const globby = require("globby");
+const yoHelper = require("@jswork/yeoman-generator-helper");
+const getp = require("@jswork/generator-prompts");
+const prompts = getp(["scope", "registry", "project_name", "description"]);
+
+module.exports = class extends Generator {
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the stunning ${chalk.red(
+          "generator-typescript"
+        )} generator!`
+      )
+    );
+    this.props = await this.prompt(prompts);
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      globby.sync(this.templatePath("**"), { dot: true }),
+      this.destinationPath(),
+      { ...this.props, ctx: yoHelper.ctx }
+    );
+  }
+};
