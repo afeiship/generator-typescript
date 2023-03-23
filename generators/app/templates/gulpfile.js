a883186bb774ae6d4585421b4be8dddfd687b36e
afeiship <1290657123@qq.com>
2023-03-23 17:09:46 +0800

    feat: use esbuild ts compile

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp');
-const { CleanRegistry, TsScripts } = require('@jswork/gulp-registry');
+const { CleanRegistry, EsbScripts } = require('@jswork/gulp-registry');
 
 const task1 = new CleanRegistry();
-const task2 = new TsScripts();
+const task2 = new EsbScripts();
 
 [task1, task2].forEach(gulp.registry);
 
-gulp.task('default', gulp.series(['clean', 'ts:scripts']));
+gulp.task('default', gulp.series(['clean', 'esb:scripts']));
