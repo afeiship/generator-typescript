2c2a331ab0a381b4c58f8e4dd7d6e166c4e48c3f
afeiship <1290657123@qq.com>
2023-03-30 18:04:33 +0800

    feat: update export mode

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,3 +1,9 @@
-export default (): void => {
+const <%- ctx.classfiy(project_name)%> = (): void => {
   console.log('hello');
 };
+
+// for es6 import
+export default <%- ctx.classfiy(project_name)%>;
+
+// for commonjs es5 require
+module.exports = <%- ctx.classfiy(project_name)%>;
