34d60ec05ddd58d3b662f61719c2973c8425d7e1
afeiship <1290657123@qq.com>
2023-08-14 09:17:22 +0800

    feat: add protofill dateformat support

--- generators/app_swc/templates/src/main.ts
+++ generators/app_swc/templates/src/main.ts
@@ -2,8 +2,9 @@ import '@/setup';
 
 class App {
   public static start() {
+    const d = new Date();
     console.log(nx.$args, nx.$conf.gets());
-    console.log('hello app: 🥬', nx.VERSION);
+    console.log('hello app: 🥬', nx.VERSION, d.foramt('datetime'));
   }
 }
 
