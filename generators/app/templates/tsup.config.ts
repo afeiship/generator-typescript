ad68a2cb5e0027a7c0cfc9b2e8accb31ffbacf2f
afeiship <1290657123@qq.com>
2023-10-31 18:06:09 +0800

    fix: exports default err

--- generators/app/templates/tsup.config.ts
+++ generators/app/templates/tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/*.ts'],
   format: ['cjs', 'esm' /*'iife' */],
+  splitting: true,
+  cjsInterop: true,
   // globalName: 'i18nHelper',
   // external: ['react'],
   dts: true,
@@ -11,5 +13,5 @@ export default defineConfig({
     return {
       js: `.${format}.js`,
     };
-  },
+  }
 });
