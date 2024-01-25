2b74954832ff33aba5bf2270a80cba23478bfa27
afeiship <1290657123@qq.com>
2024-01-25 09:39:08 +0800

    fix: cjs/esm diff d.ts

--- generators/app/templates/tsup.config.ts
+++ generators/app/templates/tsup.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   // globalName: 'i18nHelper',
   // external: ['react'],
   clean: true,
-  // dts: true,
+  dts: true,
   sourcemap: true,
-  onSuccess: 'tsc --project tsconfig.json --emitDeclarationOnly --declaration --outDir dist',
+  // onSuccess: 'tsc --project tsconfig.json --emitDeclarationOnly --declaration --outDir dist',
   outExtension({ format }) {
     return {
       js: `.${format}.js`,
